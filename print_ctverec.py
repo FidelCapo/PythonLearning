46cdf4ba8ed6c9a259fdb9939c21c8fdd5df8730
Filip Hurka <fidelcapo@Filips-MacBook-Pro.local>
2020-01-29 16:21:36 +0100

    program pro vypocet vypis obvodu ctverce
    program pro vypocet vypis obvodu ctverce

---
+++ print_ctverec.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+#program pro vypocet vypis obvodu ctverce
+strana = 10
+print("Obvod ctverce se stranou",strana,"cm je", strana * 4," cm")
+print("Obsah ctverce se stranou",strana,"cm je", strana * strana,' cm2')
