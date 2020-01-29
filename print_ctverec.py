ad4fb9d0bcb31d044d8245268ba0360c0c8c8154
Filip Hurka <fidelcapo@Filips-MacBook-Pro.local>
2020-01-29 16:43:05 +0100

    Input added

--- print_ctverec.py
+++ print_ctverec.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 #program pro vypocet vypis obvodu ctverce
-strana = 10
+
+strana = int(input("Zadej cislo: "))
+
 print("Obvod ctverce se stranou",strana,"cm je", strana * 4," cm")
 print("Obsah ctverce se stranou",strana,"cm je", strana * strana,' cm2')
